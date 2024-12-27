5831c97f39711a07dcc637f00c502a6fab5b8061
Aiyaruch Phengthong <aiyaruch1320@hotmail.com>
2024-12-27 15:09:58 +0700

    Initial mondy project

---
+++ src/App.tsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import "./App.css";
+
+interface Transaction {
+  id: number;
+  detail: string;
+  amount: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const App: React.FC = () => {
+  const [amount, setAmount] = useState<string>("");
+  const [transactions, setTransactions] = useState<Transaction[]>([
+    {
+      id: 1,
+      detail: "Salary",
+      amount: 1000,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  ]);
+
+  const addTransaction = (amount: number, type: "income" | "expenses") => {
+    const netAmount = type === "income" ? amount : -amount;
+    const newTransaction: Transaction = {
+      id: transactions.length + 1,
+      detail: "New Transaction",
+      amount: netAmount,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    setTransactions([...transactions, newTransaction]);
+    setAmount("");
+  };
+
+  const onAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    const numberValue = Number(value);
+    if (value === "" || isNaN(numberValue) || numberValue < 1) {
+      setAmount("");
+      return;
+    }
+    setAmount(value);
+  };
+
+  return (
+    <>
+      <input
+        type="number"
+        name="amount"
+        min={1}
+        onChange={onAmountChange}
+        value={amount}
+      />
+      <button
+        onClick={() => !!amount && addTransaction(Number(amount), "income")}
+      >
+        +
+      </button>
+      <button
+        onClick={() => !!amount && addTransaction(Number(amount), "expenses")}
+      >
+        -
+      </button>
+
+      {transactions.map((transaction) => (
+        <div key={transaction.id}>
+          <span>{transaction.detail}</span>
+          <span>{transaction.amount}</span>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default App;
